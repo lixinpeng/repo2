9b0837aa0d98c8ff1358ae5397bc0cd5e7a0bfad
lee <lee@163.com>
2020-04-12 17:43:54 +0800

    添加项目

---
+++ common/src/cn/study/Utills.java
@@ -0,0 +1,11 @@
+package cn.study;
+
+/**
+ * Created by Administrator on 2020/4/5.
+ */
+public class Utills {
+    public static void main(String[] args) {
+        System.out.println("测试四Asia");
+    }
+
+}
